3251f33cb3490b0d9fd22a8ed4f438371049bbd9
megahomyak <g.megahomyak@gmail.com>
2024-06-27 10:49:03 +0500

    Checkpoint; writing a Roller to improve the parsing process

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,41 +1,103 @@
-mod rest {
-    pub struct Rest<'a> {
-        s: &'a str,
-        pos: Pos,
-    }
+/*
+struct Pos {
+    pub col: usize,
+    pub row: usize,
+}
 
-    pub struct Pos {
-        pub col: usize,
-        pub row: usize,
-    }
+struct Before<'a> {
+    pub s: &'a str,
+    pub beg: Pos,
+    pub end: Pos,
+}
 
-    impl Rest<'_> {
-        pub fn trim(&mut self) -> Option<char> {
-            let mut chars = self.s.chars();
-            chars.next().inspect(|c| {
-                if *c == '\n' {
-                    self.pos.row += 1;
-                    self.pos.col = 0;
-                } else {
-                    self.pos.col += 1;
-                }
-            })
+struct After<'a> {
+    pub s: &'a str,
+    pub beg: Pos,
+}
+*/
+
+struct Roller<'a> {
+    s: &'a str,
+    idx: usize,
+}
+
+struct Roll<'a> {
+    roller: Roller<'a>,
+    c: char,
+}
+
+impl<'a> Roll<'a> {
+    fn before_idx(&self, idx: usize) -> &'a str {
+        if cfg!(debug_assertions) {
+            &self.roller.s[0..idx]
+        } else {
+            unsafe { self.roller.s.get_unchecked(0..idx) }
         }
     }
-}
-use rest::Rest;
 
-/// Distinguish string literals, as they're the most pervasive
-mod stage_1 {
-    use super::*;
-
-    pub enum Token {
-
+    fn after_idx(&self, idx: usize) -> &'a str {
+        if cfg!(debug_assertions) {
+            &self.roller.s[idx..]
+        } else {
+            unsafe { self.roller.s.get_unchecked(idx..) }
+        }
     }
 
-    fn parse(rest: Rest) -> Vec<Token> {
-        while let Some(c) = rest.trim() {
+    pub fn after(&self) -> &'a str {
+        self.after_idx(self.roller.idx)
+    }
 
+    pub fn before(&self) -> &'a str {
+        self.before_idx(self.roller.idx - self.c.len_utf8())
+    }
+
+    pub fn before_inclusive(&self) -> &'a str {
+        self.before_idx(self.roller.idx)
+    }
+
+    pub fn after_inclusive(&self) -> &'a str {
+        self.after_idx(self.roller.idx - self.c.len_utf8())
+    }
+}
+
+impl<'a> Iterator for Roller<'a> {
+    type Item = Roll<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut chars = self.after().chars();
+        chars.next().inspect(|c| {
+            self.idx += c.len_utf8();
+        })
+    }
+}
+
+fn roll(s: &str) -> Roller {
+    Roller { s, idx: 0 }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rolling() {
+        let mut roller = roll("abcdef");
+        assert_eq!(roller.next(), Some('a'));
+        assert_eq!(roller.next(), Some('b'));
+        assert_eq!(roller.next(), Some('c'));
+        assert_eq!(roller.before(), "abc");
+        assert_eq!(roller.after(), "def");
+        assert_eq!(roller.next(), Some('d'));
+        assert_eq!(roller.next(), Some('e'));
+        assert_eq!(roller.next(), Some('f'));
+        assert_eq!(roller.next(), None);
+    }
+
+    #[test]
+    fn test_parsing() {
+        let mut roller = roll("abc\"def\"ghi");
+        while let Some(c) = roller.next() {
+            if c == '"' {}
         }
     }
 }
