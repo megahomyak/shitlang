f8d43d0fb72e47b6f70df22aeda4315520d21cb3
megahomyak <g.megahomyak@gmail.com>
2024-06-29 00:04:58 +0500

    Fuck all this shit, I'm going wild with regular strings (and not excluding any info to recreate positions)

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,70 +1,135 @@
-struct Pos {
-    col: usize,
-    row: usize,
-}
+mod pos {
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct Pos {
+        col: usize,
+        row: usize,
+    }
 
-/*
-#[rustfmt::skip]
-impl Pos {
-    fn col(&self) -> usize { self.col }
-    fn row(&self) -> usize { self.row }
-}
-*/
+    const FIRST_COL: usize = 0;
 
-struct Seen<'a> {
-    s: &'a str,
-    beg: Pos,
-    end: Pos,
-}
+    impl Pos {
+        pub fn new() -> Self {
+            Self {
+                col: FIRST_COL,
+                row: 1,
+            }
+        }
 
-struct Unseen<'a> {
-    s: &'a str,
-    beg: Pos,
-}
+        pub fn advance(&mut self, c: char) {
+            if c == '\n' {
+                self.row += 1;
+                self.col = FIRST_COL;
+            } else {
+                self.col += 1;
+            }
+        }
 
-trait Slice<'a>: Sized {
-    type After;
+        pub fn advanced(s: impl Iterator<Item = char>) -> Self {
+            let mut new = Self::new();
+            for c in s {
+                new.advance(c);
+            }
+            new
+        }
 
-    fn iter(&self) -> Iter<Self>;
+        pub fn col(&self) -> usize {
+            self.col
+        }
 
-    unsafe fn split(&self, pos: Pos, idx: usize) -> (Seen<'a>, Self::After);
-}
-
-struct Iter<'a, S: Slice<'a>> {
-    slice: S,
-    idx: usize,
-    cur: Pos,
-}
-
-struct Char<I> {
-
-}
-
-impl<S: Slice> Iterator for Iter<S> {
-    type Item = (Seen, char, S::After);
-
-    fn next(&mut self) -> Option<Self::Item> {
-
+        pub fn row(&self) -> usize {
+            self.row
+        }
     }
 }
+pub use pos::Pos;
 
-impl<'a> Iterator for Seen<'a> {
-    type Item = (Seen, char, Seen);
+mod slice {
+    use super::*;
+
+    pub struct Slice<'a> {
+        s: &'a str,
+        beg: Pos,
+        end: Pos,
+    }
+
+    impl<'a> From<&'a str> for Slice<'a> {
+        fn from(s: &'a str) -> Self {
+            Self {
+                s,
+                beg: Pos::new(),
+                end: Pos::advanced(s.chars()),
+            }
+        }
+    }
+
+    impl<'a> Slice<'a> {
+        pub fn iter(&'a self) -> Iter<'a> {
+            Iter {
+                slice: self,
+                cur: self.beg,
+                idx: 0,
+            }
+        }
+    }
+
+    pub struct Iter<'a> {
+        slice: &'a Slice<'a>,
+        cur: Pos,
+        idx: usize,
+    }
+
+    impl<'a> Iter<'a> {
+        pub fn before(&self) -> Slice<'a> {
+            let s = unsafe { self.slice.s.get_unchecked(..self.idx) };
+            Slice {
+                s,
+                beg: self.slice.beg,
+                end: self.cur,
+            }
+        }
+
+        pub fn after(&self) -> Slice<'a> {
+            let s = unsafe { self.slice.s.get_unchecked(self.idx..) };
+            Slice {
+                s,
+                beg: self.slice.beg,
+                end: self.cur,
+            }
+        }
+    }
+
+    impl<'a> Iterator for Iter<'a> {
+        type Item = char;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.after().s.chars().next().inspect(|c| {
+                self.cur.advance(*c);
+                self.idx += c.len_utf8();
+            })
+        }
+    }
 }
+use slice::Slice;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_rolling() {
-        let mut roller = roll("abcdef");
-        assert_eq!(roller.next(), Some('a'));
-        assert_eq!(roller.next(), Some('b'));
-        assert_eq!(roller.next(), Some('c'));
-        assert_eq!(roller.before(), "abc");
-        assert_eq!(roller.after(), "def");
-        assert_eq!(roller.next(), Some('d'));
+    fn test_iter() {
+        let mut iter: slice::Iter = Slice::from("abc").iter();
+        assert_eq!(iter.next(), Some('a'));
+        assert_eq!(iter.next(), Some('b'));
+        assert_eq!(iter.next(), Some('c'));
+        assert_eq!(
+            iter.before(),
+            Slice {
+                s: "abc",
+                beg: Pos::new(),
+            }
+        );
+        assert_eq!(iter.after(), "def");
+        assert_eq!(iter.next(), Some('d'));
         assert_eq!(roller.next(), Some('e'));
         assert_eq!(roller.next(), Some('f'));
         assert_eq!(roller.next(), None);
