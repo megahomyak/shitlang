b9ba5df44906d5596afff15dfc6cc87c13f10d92
megahomyak <g.megahomyak@gmail.com>
2024-06-29 17:13:12 +0500

    Fucking with it still

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -20,32 +20,28 @@ struct Pos {
 }
 
 enum Error {
-    QuoteNotClosed { pos: Pos, }
+    QuoteNotClosed { pos: Pos },
 }
 
-enum Tokenizer<'a> {
-    Fucked { error: Error },
-    Right { program_parts: Vec<ProgramPart<'a>> },
+struct Tokenizer<'a> {
+    program_parts: Vec<ProgramPart<'a>>,
 }
 
 impl<'a> Tokenizer<'a> {
-    fn concretize(self, f: impl Fn(&'a str) -> Result<Vec<ProgramPart<'a>>, Error>) -> Self {
-        match self {
-            Self::Fucked { .. } => self,
-            Self::Right { program_parts } => {
-                let mut new_program_parts = Vec::new();
-                for program_part in program_parts {
-                    match program_part {
-                        ProgramPart::Token(_) => new_program_parts.push(program_part),
-                        ProgramPart::Raw(s) => new_program_parts.extend(match f(s) {
-                            Ok(new_program_parts) => new_program_parts,
-                            Err(error) => return Self::Fucked { error: Error },
-                        }),
-                    }
-                }
-                Self::Right { program_parts: new_program_parts }
+    fn concretize(
+        self,
+        f: impl Fn(&'a str) -> Result<Vec<ProgramPart<'a>>, Error>,
+    ) -> Result<Self, Error> {
+        let mut new_program_parts = Vec::new();
+        for program_part in self.program_parts {
+            match program_part {
+                ProgramPart::Token(_) => new_program_parts.push(program_part),
+                ProgramPart::Raw(s) => new_program_parts.extend(f(s)?),
             }
         }
+        Ok(Self {
+            program_parts: new_program_parts,
+        })
     }
 
     fn finalize(self, f: impl Fn(&'a str) -> Vec<Token<'a>>) -> Result<Vec<Token<'a>>, Error> {
@@ -60,7 +56,7 @@ impl<'a> Tokenizer<'a> {
                     }
                 }
                 tokens
-            })
+            }),
         }
     }
 }
