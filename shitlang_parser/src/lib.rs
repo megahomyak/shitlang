8ed9bf0310f9201a3f5142b99680ce688491c973
megahomyak <g.megahomyak@gmail.com>
2024-06-29 21:48:02 +0500

    Alright, maybe the parser combinator is not that bad... Still, the syntax is relatively small

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,14 +1,37 @@
-pub struct Offset(usize);
-pub struct Positioned<T>(Offset, T);
-
-pub struct Error {
-
+enum Expression {
+    Function(Program),
+    String(String),
+    VarName(String),
+    IfElse(Program, Program),
+    Loop(Program),
 }
 
-type Result<T> = std::iter::Peekable<std::result::Result<Positioned<T>, Positioned<Error>>>;
-
-struct Program {
-
+struct Assignment {
+    pub name: String,
+    pub value: Expression,
 }
 
-fn parse(i: Input) -> Result<Program> {}
+pub struct Program {
+    pub assignments: Vec<Assignment>,
+}
+
+pub enum Error {
+    NoNameInAssignment {
+        index: usize,
+    }
+}
+
+pub fn parse(s: &str) -> Result<Program, Error> {
+    let mut chars = s.char_indices().peekable();
+    let mut assignments = Vec::new();
+    if let Some((i, c)) = chars.peek() {
+        if *c == '=' {
+            return Err(Error::NoNameInAssignment { index: *i });
+        } else if c.is_whitespace() {}
+        else {
+        
+        }
+    } else {
+        return Ok(Program { assignments });
+    }
+}
