ad57e064713ede7980683f04d0d79b9c8eb5f9b0
megahomyak <g.megahomyak@gmail.com>
2024-06-30 00:04:55 +0500

    Checkpoint before removing `struct Input`

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,13 +1,56 @@
-enum Expression {
-    Function(Program),
-    String(String),
-    VarName(String),
-    IfElse(Program, Program),
-    Loop(Program),
+type Input<'a> = std::iter::Peekable<std::str::CharIndices<'a>>;
+
+#[derive(Clone, Copy)]
+struct Input<'a> {
+    s: &'a str,
+    index: usize,
+    c: Option<char>,
 }
 
-struct Assignment {
-    pub name: String,
+impl<'a> Input<'a> {
+    fn advance(&mut self) {
+        if let Some(c) = self.c {
+            self.index += c.len_utf8();
+            self.c = self.s.get(self.index..).map;
+        }
+    }
+}
+
+pub struct Function {
+    pub content: Program,
+}
+
+pub struct IfElse {
+    pub if_branch: Program,
+    pub else_branch: Program,
+}
+
+pub struct Loop {
+    pub content: Program,
+}
+
+pub struct VarName {
+    pub content: String,
+}
+
+impl VarName {
+    pub fn parse(input: Input) -> Result<Self> {}
+}
+
+pub struct ShitString {
+    pub content: String,
+}
+
+pub enum Expression {
+    Function(Function),
+    String(ShitString),
+    VarName(VarName),
+    IfElse(IfElse),
+    Loop(Loop),
+}
+
+pub struct Assignment {
+    pub name: VarName,
     pub value: Expression,
 }
 
@@ -16,22 +59,38 @@ pub struct Program {
 }
 
 pub enum Error {
-    NoNameInAssignment {
-        index: usize,
+    NoNameInAssignment { index: Index },
+}
+
+pub type Result<'a, T, E = Option<Error>> = std::result::Result<(T, Input<'a>), E>;
+
+fn skip_whitespace(mut input: Input) -> Input {
+    while let Some(c) = input.c {
+        if !c.is_whitespace() {
+            break;
+        }
+        input.advance();
+    }
+    input
+}
+
+impl Program {
+    fn parse(input: Input) -> Result<Self, Error> {
+        let input = skip_whitespace(input);
+        let Ok((var_name, input)) = VarName::parse(input) else {
+            return Err(Error::NoNameInAssignment {
+                index: input.index,
+            });
+        };
+        let input = skip_whitespace(input);
+        let mut chars = input.chars();
+        match chars.next() {
+            '=' => (),
+            _ => return,
+        }
     }
 }
 
-pub fn parse(s: &str) -> Result<Program, Error> {
-    let mut chars = s.char_indices().peekable();
-    let mut assignments = Vec::new();
-    if let Some((i, c)) = chars.peek() {
-        if *c == '=' {
-            return Err(Error::NoNameInAssignment { index: *i });
-        } else if c.is_whitespace() {}
-        else {
-        
-        }
-    } else {
-        return Ok(Program { assignments });
-    }
+pub fn parse(input: &str) -> std::result::Result<Program, Error> {
+    Program::parse(input).map(|(program, _rest)| program)
 }
