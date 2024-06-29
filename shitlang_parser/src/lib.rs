f9f36fcfe9ff1605ea5ec0266e2f8cc33ac2bd45
megahomyak <g.megahomyak@gmail.com>
2024-06-30 03:18:59 +0500

    A big restructuring

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,40 +1,87 @@
-type Input<'a> = std::iter::Peekable<std::str::CharIndices<'a>>;
+mod input {
+    #[derive(Clone)]
+    pub struct Input<'a> {
+        iter_output: Option<(usize, char)>,
+        iter: std::str::CharIndices<'a>,
+    }
 
-#[derive(Clone, Copy)]
-struct Input<'a> {
-    s: &'a str,
-    index: usize,
-    c: Option<char>,
-}
+    impl<'a> Input<'a> {
+        pub fn new(s: &'a str) -> Self {
+            let mut iter = s.char_indices();
+            Self {
+                iter_output: iter.next(),
+                iter,
+            }
+        }
 
-impl<'a> Input<'a> {
-    fn advance(&mut self) {
-        if let Some(c) = self.c {
-            self.index += c.len_utf8();
-            self.c = self.s.get(self.index..).map;
+        pub fn peek(&self) -> Option<(usize, char)> {
+            self.iter_output
+        }
+
+        pub fn advance(&mut self) {
+            self.iter_output = self.iter.next();
         }
     }
 }
+use input::Input;
 
-pub struct Function {
-    pub content: Program,
+#[derive(Clone)]
+pub enum Position {
+    ByteOffset(usize),
+    EndOfFile,
 }
 
-pub struct IfElse {
-    pub if_branch: Program,
-    pub else_branch: Program,
+pub mod function {
+    pub enum Error {
+
+    }
+
+    pub struct Function {
+        pub content: Program,
+    }
+}
+pub use function::Function;
+
+pub mod if_else {
+    pub enum Error {}
+
+    pub struct IfElse {
+        pub if_branch: Program,
+        pub else_branch: Program,
+    }
+}
+pub use if_else::IfElse;
+
+pub mod loop {
+    pub enum Error {}
+
+    pub struct Loop {
+        pub content: Program,
+    }
 }
 
-pub struct Loop {
-    pub content: Program,
-}
-
-pub struct VarName {
+pub struct VariableName {
     pub content: String,
 }
 
-impl VarName {
-    pub fn parse(input: Input) -> Result<Self> {}
+impl VariableName {
+    pub fn parse(mut input: Input) -> ShitResult<Self, ()> {
+        let mut content = String::new();
+        while let Some((_i, c)) = input.peek() {
+            match c {
+                '"' | '=' => break,
+                c if c.is_whitespace() => break,
+                c => content.push(c),
+            }
+            input.advance();
+        }
+        if content.is_empty() {
+            Err(())
+        } else {
+            content.shrink_to_fit();
+            Ok((Self { content }, input))
+        }
+    }
 }
 
 pub struct ShitString {
@@ -44,13 +91,13 @@ pub struct ShitString {
 pub enum Expression {
     Function(Function),
     String(ShitString),
-    VarName(VarName),
+    VariableName(VariableName),
     IfElse(IfElse),
     Loop(Loop),
 }
 
 pub struct Assignment {
-    pub name: VarName,
+    pub name: VariableName,
     pub value: Expression,
 }
 
@@ -58,14 +105,23 @@ pub struct Program {
     pub assignments: Vec<Assignment>,
 }
 
-pub enum Error {
-    NoNameInAssignment { index: Index },
+pub enum AssignmentError
+
+pub enum ErrorKind {
+    AssignmentError(AssignmentError)
+    MissingNameInAssignment,
+    MissingEqualsSignInAssignment,
 }
 
-pub type Result<'a, T, E = Option<Error>> = std::result::Result<(T, Input<'a>), E>;
+pub struct Error {
+    pub range: std::ops::RangeInclusive<Position>,
+    pub kind: ErrorKind,
+}
+
+pub type ShitResult<'a, T, E> = Result<(T, Input<'a>), E>;
 
 fn skip_whitespace(mut input: Input) -> Input {
-    while let Some(c) = input.c {
+    while let Some((_i, c)) = input.peek() {
         if !c.is_whitespace() {
             break;
         }
@@ -74,23 +130,32 @@ fn skip_whitespace(mut input: Input) -> Input {
     input
 }
 
+fn make_range<T: Clone>(item: T) -> std::ops::RangeInclusive<T> {
+    item.clone()..=item
+}
+
 impl Program {
-    fn parse(input: Input) -> Result<Self, Error> {
+    fn parse(input: Input) -> ShitResult<Self, Error> {
         let input = skip_whitespace(input);
-        let Ok((var_name, input)) = VarName::parse(input) else {
-            return Err(Error::NoNameInAssignment {
-                index: input.index,
+        let Ok((var_name, input)) = VariableName::parse(input.clone()) else {
+            return Err(Error {
+                range: make_range(match input.peek() {
+                    Some((i, _c)) => Position::ByteOffset(i),
+                    None => Position::EndOfFile,
+                }),
+                kind: ErrorKind::MissingNameInAssignment,
             });
         };
         let input = skip_whitespace(input);
-        let mut chars = input.chars();
-        match chars.next() {
-            '=' => (),
-            _ => return,
+        match input.peek() {
+            None => Err(Error {
+                range: make_range(Position::EndOfFile),
+                kind: ErrorKind::
+            }),
         }
     }
 }
 
-pub fn parse(input: &str) -> std::result::Result<Program, Error> {
-    Program::parse(input).map(|(program, _rest)| program)
+pub fn parse(input: &str) -> Result<Program, Error> {
+    Program::parse(Input::new(input)).map(|(program, _rest)| program)
 }
