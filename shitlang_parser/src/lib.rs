f34a64e6a7acf2872dbadcfa73d3cb0410c9d190
megahomyak <g.megahomyak@gmail.com>
2024-06-30 09:05:30 +0500

    Trying to write macros, failing

--- shitlang_parser/src/lib.rs
+++ shitlang_parser/src/lib.rs
@@ -1,119 +1,159 @@
-type Input<'a> = std::str::CharIndices<'a>;
+mod input {
+    #[derive(Clone, Copy)]
+    pub struct Input<'a> {
+        s: &'a str,
+        index: usize,
+    }
 
-#[derive(Clone)]
-pub enum Position {
-    ByteOffset(usize),
-    EndOfFile,
-}
+    impl<'a> Input<'a> {
+        pub fn new(s: &'a str) -> Self {
+            Self { s, index: 0 }
+        }
 
-pub struct Function {
-    pub content: Program,
-}
-
-pub enum FunctionError {}
-
-pub struct IfElse {
-    pub if_branch: Program,
-    pub else_branch: Program,
-}
-
-pub enum IfElseError {}
-
-pub struct Loop {
-    pub content: Program,
-}
-
-pub enum LoopError {}
-
-pub struct Name {
-    pub content: String,
-}
-
-fn parse_string_delimiter(c: char, input: Input) -> bool {
-
-}
-
-fn parse_assignment_sign(c: char, input: Input) -> ShitResult<(), ()> {
-    parse_char(input, '=')
-}
-
-const END: &str = "end";
-
-fn any_matches<const N: usize>(c: char, input: Input, parsers: [fn (char, Input) -> bool; N]) -> bool {
-    for parser in &parsers {
-        if parser(c, input.clone()) {
-            return false;
+        pub fn next(&mut self) -> Option<(usize, char)> {
+            unsafe { self.s.get_unchecked(self.index..) }
+                .chars()
+                .next()
+                .map(|c| (self.index, c))
+                .inspect(|(_i, c)| self.index += c.len_utf8())
         }
     }
-    true
 }
+use input::Input;
 
-fn parse_word_char(mut input: Input) -> ShitResult<char, ()> {
-    let mut initial_input = input;
-    match input.next() {
-        None => (),
-        Some((i, c)) => {
-            if !c.is_whitespace() {
-                return Ok((c, ))
+mod utils {
+    use super::*;
+
+    pub fn parse_char(mut input: Input, c: char) -> ShitResult<(), ()> {
+        match input.next() {
+            None => (),
+            Some((_i, input_c)) => {
+                if input_c == c {
+                    return Ok(((), input));
+                }
             }
         }
-    }
-    Err(())
-    if any_matches(initial_input.clone(), [parse_string_delimiter, parse_assignment_sign]) || c.is_whitespace() {
         Err(())
-    } else {
-        Ok((input.next(), input))
     }
-}
 
-fn is_at_word_boundary(current_character: char, input: Input) -> bool {
-}
-
-fn parse_known_word(mut input: Input, word: &str) -> Option<Input> {
-    for c in word.chars() {
-        input = parse_char(input, c)?;
+    pub fn parse_word_char(mut input: Input) -> ShitResult<char, ()> {
+        if ShitString::parse_delimiter(input.clone()).is_err()
+            && Assignment::parse_separator(input.clone()).is_err()
+        {
+            match input.next() {
+                None => (),
+                Some((_i, c)) => {
+                    if !c.is_whitespace() {
+                        return Ok((c, input));
+                    }
+                }
+            }
+        }
+        Err(())
     }
-    match input.peek() {
-        None => Some(input),
-        Some((i, c)) => {
-            if is_at_word_boundary(c, input) {}
-            input.advance();
+
+    pub fn parse_known_word<'a>(mut input: Input<'a>, word: &str) -> ShitResult<'a, (), ()> {
+        for word_c in word.chars() {
+            match input.next() {
+                None => (),
+                Some((_i, c)) => {
+                    if word_c == c {
+                        continue;
+                    }
+                }
+            }
+            return Err(());
+        }
+        match parse_word_char(input.clone()) {
+            Err(()) => Ok(((), input)),
+            Ok(_) => Err(()),
         }
     }
-}
 
-fn parse_unknown_word(mut input: Input) -> ShitResult<String, ()> {
-    let mut name = String::new();
-    while let Some((_i, c)) = input.peek() {
-        if any_matches(input.clone(), [parse_string_delimiter, parse_assignment_sign]) || c.is_whitespace() {
-            break;
+    #[derive(Clone)]
+    pub enum Position {
+        ByteOffset(usize),
+        EndOfFile,
+    }
+}
+pub use utils::Position;
+use utils::*;
+
+macro_rules! shit_mod {
+    ($name:ident exports $export:ident { $body:tt }) => {
+        pub mod $name {
+            use super::*;
+
+            $body
         }
-        name.push(c);
-        input.advance();
-    }
-    if name.is_empty() {
-        Err(())
-    } else {
-        name.shrink_to_fit();
-        Ok((name, input))
+        pub use $name::$export;
     }
 }
 
-impl Name {
-    fn parse()
-}
+shit_mod! {function exports Function {
+    pub struct Function {
+        pub content: Program,
+    }
+
+    pub enum Error {}
+}}
+
+shit_mod! {if_else exports IfElse {
+    pub struct IfElse {
+        pub if_branch: Program,
+        pub else_branch: Program,
+    }
+
+    pub enum Error {}
+}}
+
+shit_mod! {shit_loop exports Loop {
+    use super::*;
+
+    pub struct Loop {
+        pub content: Program,
+    }
+
+    pub enum LoopError {}
+}}
+
+shit_mod! {name exports Name {
+    use super::*;
+
+    pub struct Name {
+        pub content: String,
+    }
+
+    pub(super) fn parse(input: Input) -> ShitResult {}
+
+    impl Name {
+        fn parse(input: Input) -> ShitResult<Self, ()> {}
+    }
+}}
 
 pub enum NameError {}
+
+// String
 
 pub struct ShitString {
     pub content: String,
 }
 
+impl ShitString {
+    fn parse_delimiter(input: Input) -> ShitResult<(), ()> {
+        utils::parse_char(input, '"')
+    }
+}
+
 pub enum StringError {}
+
+// Import
 
 pub struct Import {}
 
 pub enum ImportError {}
+
+// Expression
 
 pub enum Expression {
     Function(Function),
@@ -124,6 +164,8 @@ pub enum Expression {
     Import(Import),
 }
 
+// Assignment
+
 pub struct Assignment {
     pub name: Name,
     pub value: Expression,
@@ -131,42 +173,27 @@ pub struct Assignment {
 
 pub enum AssignmentError {
     MissingName,
-    MissingAssignmentSign,
+    MissingAssignmentSeparator,
     MissingExpression,
 }
 
-fn parse_keyword(input: Input, keyword: &'static str) -> Option<Input> {
+impl Assignment {
+    fn parse_separator(input: Input) -> ShitResult<(), ()> {
+        parse_char(input, '=')
+    }
 
+    fn parse(input: Input) -> ShitResult<Self, Option<AssignmentError>> {}
 }
 
-impl Assignment {
-    fn parse(input: Input) -> ShitResult<Self, Option<AssignmentError>> {
-        let input = skip_whitespace(input);
-        let (var_name, input) = match Name::parse(input.clone()) {
-            Ok((var_name, input)) =>
-            Err(()) => if 
-        }
-        let Ok((var_name, input)) = Name::parse(input.clone()) else {
-            return Err(Error {
-                range: one_item_range(match input.peek() {
-                    Some((i, _c)) => Position::ByteOffset(i),
-                    None => Position::EndOfFile,
-                }),
-                kind: AssignmentError::MissingName,
-            });
-        };
-        let input = skip_whitespace(input);
-        match input.peek() {
-            None => Err(Error {
-                range: one_item_range(Position::EndOfFile),
-                kind: ProgramError::
-            }),
-        }
-    }
+// Statement
+
+pub enum Statement {
+    Assignment(Assignment),
+    Expression(Expression),
 }
 
 pub struct Program {
-    pub assignments: Vec<Assignment>,
+    pub statements: Vec<Statement>,
 }
 
 pub struct Error<Kind> {
@@ -177,13 +204,17 @@ pub struct Error<Kind> {
 type ShitResult<'a, T, E> = Result<(T, Input<'a>), E>;
 
 fn skip_whitespace(mut input: Input) -> Input {
-    while let Some((_i, c)) = input.peek() {
-        if !c.is_whitespace() {
-            break;
+    loop {
+        let input_backup = input.clone();
+        match input.next() {
+            None => return input,
+            Some((_i, c)) => {
+                if !c.is_whitespace() {
+                    return input_backup;
+                }
+            }
         }
-        input = input.advanced();
     }
-    input
 }
 
 fn one_item_range<T: Clone>(item: T) -> std::ops::RangeInclusive<T> {
@@ -191,11 +222,12 @@ fn one_item_range<T: Clone>(item: T) -> std::ops::RangeInclusive<T> {
 }
 
 impl Program {
+    fn parse_end_marker(input: Input) -> ShitResult<(), ()> {
+        parse_known_word(input, "end")
+    }
+
     fn parse(input: Input) -> ShitResult<Program, Error<AssignmentError>> {
         let mut assignments = Vec::new();
-        loop {
-
-        }
     }
 }
 
